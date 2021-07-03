eed2ae3cbc107a013946b2f94e377cb0937fecf5
OriginCode <self@origincode.me>
2021-07-03 21:48:21 +0800

    meta: format code

--- src/main.rs
+++ src/main.rs
@@ -26,20 +26,19 @@ fn fetch_aqi(token: String, city: &str) -> Result<APIData> {
         token,
     );
     let resp = Client::new().get(&url).send()?;
-    
+
     match resp.json::<APIData>() {
         Ok(d) => Ok(d),
-        Err(_) => Err(anyhow!("Failed to process the data"))
+        Err(_) => Err(anyhow!("Failed to process the data")),
     }
 }
 
 fn main() -> Result<()> {
     let args: Vec<String> = env::args().collect();
-    let city = args.get(1).ok_or_else(|| anyhow!("Please enter a city name"))?;
-    let fetched = fetch_aqi(
-        env::var("TOKEN")?,
-        city,
-    )?;
+    let city = args
+        .get(1)
+        .ok_or_else(|| anyhow!("Please enter a city name"))?;
+    let fetched = fetch_aqi(env::var("TOKEN")?, city)?;
 
     println!(
         "{}: {} ({})",
